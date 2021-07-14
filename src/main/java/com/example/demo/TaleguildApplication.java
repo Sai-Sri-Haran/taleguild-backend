48593dcd5f224f628dd4fd733c6b94a0ad7830c3
ragavanRagav <ragavan111999@gmail.com>
2021-07-14 11:46:27 +0530

    cors error fix

--- src/main/java/com/example/demo/TaleguildApplication.java
+++ src/main/java/com/example/demo/TaleguildApplication.java
@@ -6,13 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
+@Configuration
 public class TaleguildApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaleguildApplication.class, args);
 	}
 
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*").allowedMethods("");
+    }
+	
 }
 
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.example.demo.Service.UserService;
 import com.example.demo.model.UserModel;
 import com.example.demo.repository.UserRepository;
 
+@CrossOrigin
 @RestController
 public class UserController {
 	
